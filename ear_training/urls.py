bc8e3e4c2ceb01705ea1eeeb0d227fbdadba2e1c
Archit Choudhary <thearchitc@gmail.com>
2023-08-06 13:45:22 +0200

    New landing page for training exercises (also play -> train)

--- ear_training/urls.py
+++ ear_training/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 app_name = 'ear_training'
 urlpatterns = [
+    path('', views.home, name='home'),
+    path('help/', views.help, name='help'),
     path('chord_quality/', views.chord_quality, name='chord_quality'),
-    path('help/', views.help, name='help')
+    path('rhythmic_dictation/', views.rhythmic_dictation, name='rhythmic_dictation'),
 ]
--- ear_training/views.py
+++ ear_training/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
 
-# Create your views here.
-def chord_quality(request):
-    return render(request, 'ear_training/chord_quality.html')
+
+def home(request):
+    return render(request, 'ear_training/train_home.html')
 
 
 def help(request):
     return render(request, 'ear_training/help.html')
+
+
+def chord_quality(request):
+    return render(request, 'ear_training/chord_quality.html')
+
+
+def rhythmic_dictation(request):
+    return render(request, 'ear_training/rhythmic_dictation.html')
