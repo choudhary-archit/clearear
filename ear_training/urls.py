e312ae743b4f18b9e131d195fc1f79ba9499ecbc
Archit Choudhary <thearchitc@gmail.com>
2023-08-06 13:26:49 +0200

    Added a help page, because things are about to go crazee

--- ear_training/urls.py
+++ ear_training/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 app_name = 'ear_training'
 urlpatterns = [
-    path('chord_quality/', views.chord_quality, name='chord_quality')
+    path('chord_quality/', views.chord_quality, name='chord_quality'),
+    path('help/', views.help, name='help')
 ]
--- ear_training/views.py
+++ ear_training/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def chord_quality(request):
     return render(request, 'ear_training/chord_quality.html')
+
+
+def help(request):
+    return render(request, 'ear_training/help.html')
